e80a773dbb5f04bfbf485d3a7917c4ae1999140d
Malcolm Greene <mgreene@bdurham.com>
2018-02-21 13:09:56 -0700

    Updates to support generic setups not tied to specific exe versions

--- python_setup.cmd
+++ python_setup.cmd
@@ -2,7 +2,7 @@
 REM python_setup.cmd
 REM Install Python for all users in \udp\python; copy helper scripts to C:\Windows
 REM Options described at https://docs.python.org/3/using/windows.html
-..\downloads\python-3.6.4-amd64.exe  /quiet  InstallAllUsers=1  DefaultAllUsersTargetDir=c:\udp\python  PrependPath=1 
+..\downloads\python-setup.exe  /quiet  InstallAllUsers=1  DefaultAllUsersTargetDir=c:\udp\python  PrependPath=1 
 copy /Y idle.cmd    c:\windows > nul
 copy /Y pip.cmd     c:\windows > nul
 copy /Y python.cmd  c:\windows > nul
--- python_verify.cmd
+++ python_verify.cmd
@@ -1,12 +1,12 @@
 @echo off
-REM python_verify.cmd - verifies Python setup
+REM python_verify.cmd 
+REM Verifies Python setup
 
-echo PYTHON:              > python_verify.txt
-python_verify.py         >> python_verify.txt
-echo.                    >> python_verify.txt
-echo ASSOC:              >> python_verify.txt
-assoc | find /I "python" >> python_verify.txt
-echo.                    >> python_verify.txt
-echo FTYPE:              >> python_verify.txt
-ftype | find /I "python" >> python_verify.txt
-notepad python_verify.txt
+echo PYTHON:            
+python_verify.py        
+echo.                   
+echo ASSOC:             
+assoc | find /I "python"
+echo.                   
+echo FTYPE:             
+ftype | find /I "python"
