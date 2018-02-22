78d9301ec4a0c1009086a37ded4d275063637ddc
Malcolm Greene <mgreene@bdurham.com>
2018-02-22 02:14:54 -0700

    RDS tests

--- capture.py
+++ capture.py
@@ -1,4 +1,4 @@
-# pyodbc_verify.py
+# capture.py
 
 '''
 Verify pyodbc working with Alterra databases.
@@ -37,6 +37,11 @@ Ref Prod
 AMP Utility UAT
 VM-DED-SQLC1N1.ILAB.TST\DEV,49733 - AMPUtility
 
+RDS (dbo.SaleDates, dbo.Meals)
+server: udp-powerbi-sandbox.c4yiy3ssz15k.us-west-2.rds.amazonaws.com
+database: Sandy
+user: udp_powerbi_sandbox_admin
+pass: PastrySportAnywhere
 
 '''
 
@@ -56,6 +61,25 @@ def test_table_pull(cursor):
     for row in cursor:
         # print(row.transactionid, row.job_id)
         pass
+
+
+# pull a table, save it to disk as a CSV
+def pull_table(table_name, job_id):
+    sql_command = f'select *, {job_id} as job_id from {table_name}'
+    cursor.execute(sql_command)
+
+    col_names = [col[0] for col in cursor.description]
+    print(col_names)
+
+    '''    
+    print(cursor.description)
+    for col in cursor.description:
+        print(col[0])
+        
+    for row in cursor:
+        # print(row.transactionid, row.job_id)
+        pass
+    '''
     
 
 # if you are running on Windows, you can just use the native driver: driver={SQL Server}
@@ -67,22 +91,35 @@ db_driver = '{SQL Server}'
 db_server = r''
 db_database = r''
 db_user = r''
-db_pass = ''
+db_pass = r''
 db_app = r'message visible to SQL Server admin'
 
 db_server = r'VM-DED-SQLC1N1.ILAB.TST\DEV,49733'
 db_database = r'RTPIkon'
-
 
 # build connection string
 db_connection = list()
 db_connection.append(f'driver={db_driver}')
 db_connection.append(f'server={db_server}')
 db_connection.append(f'database={db_database}')
-
-db_connection.append('trusted_connection=yes')
+db_connection.append(f'trusted_connection=yes')
 # db_connection.append(f'uid={db_user}')
 # db_connection.append(f'pwd={db_pass}')
+
+'''
+# RDS sandbox tests
+db_server = r'udp-powerbi-sandbox.c4yiy3ssz15k.us-west-2.rds.amazonaws.com'
+db_database = r'Sandy'
+db_user = r'udp_powerbi_sandbox_admin'
+db_pass = r'PastrySportAnywhere'
+db_connection = list()
+db_connection.append(f'driver={db_driver}')
+db_connection.append(f'server={db_server}')
+db_connection.append(f'database={db_database}')
+db_connection.append(f'uid={db_user}')
+db_connection.append(f'pwd={db_pass}')
+'''
+
 
 # establish connection
 db_connection_str = '; '.join(db_connection)
@@ -96,7 +133,16 @@ cursor = conn.cursor()
 cursor.fast_executemany = True
 
 # test pull
-test_table_pull(cursor)
+# pull_table()
+
+'''
+# RDS sandbox tests
+sql_command = f'select * from dbo.Meals'
+sql_command = f'select * from dbo.SaleDates'
+cursor.execute(sql_command)
+for row in cursor:
+    print(row)
+'''
 
 # cleanup
 cursor.close()
